64f3401152ef80ddf1fa8002c6ef0adea168498a
Irfan <79147749+irfantaskopru@users.noreply.github.com>
2023-02-18 22:39:21 +0300

    Update strings.py

--- String Example/strings.py
+++ String Example/strings.py
@@ -1,6 +1,3 @@
-"""
-Preserve the Verse has one task for you. They have delivered you a string that contains a list of poems, titled highlighted_poems, they want to highlight on the site, but they need your help to parse the string into something that can display the name, title, and publication date of the highlighted poems on the site.
-"""
 highlighted_poems = "Afterimages:Audre Lorde:1997,  The Shadow:William Carlos Williams:1915, Ecstasy:Gabriela Mistral:1925,   Georgia Dusk:Jean Toomer:1923,   Parting Before Daybreak:An Qi:2014, The Untold Want:Walt Whitman:1871, Mr. Grumpledump's Song:Shel Silverstein:2004, Angel Sound Mexico City:Carmen Boullosa:2013, In Love:Kamala Suraiyya:1965, Dream Variations:Langston Hughes:1994, Dreamwood:Adrienne Rich:1987"
 
 """The information for each poem is separated by commas, and within this information is the title of the poem, the author, and the date of publication."""
